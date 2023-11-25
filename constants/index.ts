a201681c2cdc447dd48cb56de737420547731010
Abdillah Mufki Auzan Mubin <abdillahmufky123@gmail.com>
2023-11-25 14:10:57 +0700

    update

---
+++ constants/index.ts
@@ -0,0 +1,89 @@
+export const PROJECT_DATA = [
+  {
+    id: 1,
+    title: "Web KuyTravel",
+    description:
+      "The KuyTravel website is a modern, responsive and feature-rich website. This website was built using modern and latest technologies such as NextJs, TypeScript, Tailwind.",
+    src: "/project/kuyTravel.png",
+    link: "https://kuy-travel.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Mobile Apps Find Job",
+    description:
+      "This innovative solution combines a user-friendly interface with seamless functionality, offering a modern platform for those seeking their dream career. Explore my portfolio to see how React Native Expo is driving the development of dynamic job search apps, making a big impact in mobile development.",
+    src: "/project/kuyTravel.png",
+    link: "https://github.com/abdillahmufki/Job_Portal_RN",
+  },
+  {
+    id: 3,
+    title: "Frontend Digital CV",
+    description:
+      "Online job portal website application that connects talented professionals like you with exciting career opportunities. Developed using a robust HTML, CSS, JavaScript, Bootstrap and jQuery stack, DigitalCV offers a seamless and easy-to-use experience for job seekers and employers alike.",
+    src: "/project/kuyTravel.png",
+    link: "https://digitalcv.id/id/candidate/index.php",
+  },
+  {
+    id: 4,
+    title: "Company Profile Gestalt Systech",
+    description:
+      "I am very proud to display my skills as a developer specializing in creating captivating company profiles for software houses and IT consulting firms. One of my notable projects involved collaborating with Gestalt Systech, a leading name in the industry, to develop a stunning company profile using React.js and Tailwind CSS.",
+    src: "/project/kuyTravel.png",
+    link: "https://gestaltsystech.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "GPT-3 Clone ReactJS",
+    description:
+      "GPT-3 is a clone of OpenAI, this project is a learning media javscript library for creating compnent based user intefaces, using ReactJS",
+    src: "/project/kuyTravel.png",
+    link: "https://github.com/abdillahmufki/Modern-UI-UX-Website-in-React-JS",
+  },
+  {
+    id: 5,
+    title: "Company Profile PT. Buana Selaras Berjaya",
+    description:
+      "Our impressive company profile project utilizes the power of React.js and Tailwind CSS to create a captivating online presence for your business. With a sleek design, responsive layout, and seamless functionality, this project showcases your company's strengths and offerings in a modern and engaging way.",
+    src: "/project/kuyTravel.png",
+    link: "http://buanaselarasberjaya.com/",
+  },
+  {
+    id: 6,
+    title: "KuyNgemils",
+    description:
+      "Built using the powerful React.js technology, KuyNgemils offers fast and responsive performance. You can explore the various available food menus smoothly, view food details, set personal preferences, and track orders easily. In addition, with an attractive and intuitive design, this application provides a pleasing user experience that pleases your eyes.",
+    src: "/project/kuyTravel.png",
+    link: "https://foodorderapp-delta.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "User Interface HealingKuy",
+    description: "UI about booking a place to stay using Figma.",
+    src: "/project/kuyTravel.png",
+    link: "https://www.figma.com/file/oQblEaGpy4ncgr6nNy2u9O/Staycation?type=design&node-id=0%3A1&mode=design&t=AvzRJYbMGD2a8ses-1",
+  },
+  {
+    id: 8,
+    title: "User Interface Smart Parking",
+    description:
+      "UI Admin Smart Parking, my project which manages parking data based on smart parking, using Figma tool.",
+    src: "/project/kuyTravel.png",
+    link: "https://www.figma.com/file/Sy244en5iDel78FRxqfYlv/Arkom?type=design&mode=design&t=AvzRJYbMGD2a8ses-1",
+  },
+  {
+    id: 9,
+    title: "Web Bolabol",
+    description:
+      "Bolabol is a website for football lovers, where football fans can see clubs, players, managers, and stadiums. This website was created using Laravel's Eloquent ORM.",
+    src: "/project/kuyTravel.png",
+    link: "https://github.com/abdillahmufki/Bolabol",
+  },
+  {
+    id: 10,
+    title: "Management Kecamatan",
+    description:
+      "In this project, I created CRUD on the Management Kecamatan feature, with the CodeIgniter 4 Framework.",
+    src: "/project/kuyTravel.png",
+    link: "https://github.com/abdillahmufki/Project-Uas-Management-kecamatan",
+  },
+];
