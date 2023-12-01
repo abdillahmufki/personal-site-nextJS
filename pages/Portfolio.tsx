8fb75b5a223890ef07389fc79204a43639d8629b
Abdillah Mufki Auzan Mubin <abdillahmufky123@gmail.com>
2023-12-01 17:06:26 +0700

    update

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -3,6 +3,13 @@ import Tab from "@/components/Tab";
 const Portfolio = () => {
   return (
     <section id="portfolio">
+      {" "}
+      <div className="flex justify-center flex-col items-center mt-5 mb-[50px]">
+        <h2 className="text-4xl font-semibold text-gray-20 dark:text-white">
+          Portfolio
+        </h2>
+        <p className="text-gray-20 dark:text-white text-sm">Most recent work</p>
+      </div>
       <div className="flex justify-center items-center">
         <div>
           <Tab />
