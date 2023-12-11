22eabb5c71fdcb35588bd7e0017e4f586654cae4
Abdillah Mufki Auzan Mubin <abdillahmufky123@gmail.com>
2023-12-11 11:08:50 +0700

    update

--- components/Portfolio/Website.tsx
+++ components/Portfolio/Website.tsx
@@ -6,7 +6,7 @@ import Pagination from "../Pagination";
 
 import { PROJECT_DATA_WEBSITE } from "@/constants";
 
-interface Projects {
+interface Product {
   title: string;
   description: string;
   link: string;
@@ -18,7 +18,7 @@ const Website: React.FC = () => {
   const itemsPerPage: number = 6; // Number of items per page
 
   // Get data for the current page
-  const paginatedData: Projects[] = PROJECT_DATA_WEBSITE.slice(
+  const paginatedData: Product[] = PROJECT_DATA_WEBSITE.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
@@ -37,34 +37,26 @@ const Website: React.FC = () => {
   return (
     <div className="my-10 flex justify-center flex-col lg:my-10 p-5">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5">
-        {paginatedData.map((project: Projects, index: number) => (
-          <div key={index} className="flex items-center justify-center">
-            <Link
-              className="relative block w-[350px] h-[350px] bg-gray-900 group"
-              href={project.link}>
-              <Image
-                className="absolute inset-0 object-cover w-full h-full group-hover:opacity-50 rounded-md"
-                src={project.src}
-                alt="image"
-                width={350}
-                height={350}
-              />
-              <div className="relative p-5">
-                <div className="mt-40">
-                  {/* Hidden content */}
-                  <div
-                    className="transition-all transform 
-                                translate-y-8 opacity-0 
-                                group-hover:opacity-100 
-                                group-hover:translate-y-0">
-                    <div className="p-2">
-                      <p className="text-lg text-white mb-2">{project.title}</p>
-                      <button className="px-4 py-2 text-sm text-white badge badge-primary">
-                        Design
-                      </button>
-                    </div>
-                  </div>
-                  {/* End of hidden content */}
+        {paginatedData.map((product: Product, index: number) => (
+          <div key={index}>
+            <Link target="_blank" href={product.link} className="p-1">
+              <div className="group relative block overflow-hidden rounded-md transition-all duration-500">
+                <div className="lightbox tobii-zoom transition-all duration-500  shadow-md group-hover:brightness-50 group-hover:scale-105">
+                  <Image
+                    width={350}
+                    height={300}
+                    className="w-[350px] h-[350px]"
+                    alt="img"
+                    src={product.src}
+                  />
+                </div>
+                <div className="absolute flex flex-col gap-2 -bottom-52 left-2 right-2 rounded p-4 transition-all duration-500 group-hover:bottom-2 bg-transparent">
+                  <Link
+                    href={product.link}
+                    className="hover:text-primary-600 text-white dark:white text-lg font-medium transition duration-500">
+                    {product.title}
+                  </Link>
+                  <div className="badge badge-primary">Application</div>
                 </div>
               </div>
             </Link>
