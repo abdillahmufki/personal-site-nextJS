125ffc239ae142aea38faa8c37f6642c9aae2282
Abdillah Mufki Auzan Mubin <abdillahmufky123@gmail.com>
2024-01-02 15:05:05 +0700

    update

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const projectId = "3m6hsdp9";
+const dataset = "production";
+const apiVersion = "2022-03-07";
 
 export const client = createClient({
   projectId,
